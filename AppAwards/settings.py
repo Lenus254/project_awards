5ed8a9f0537bd88ad8a38d09e8417478c45b3c79
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-13 18:28:21 +0300

    registered awards app

--- AppAwards/settings.py
+++ AppAwards/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'awards'
+    'bootstrap4'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

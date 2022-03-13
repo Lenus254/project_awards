da3b1cb4504327023333c610bacaca30b403136e
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-13 21:26:47 +0300

    defined url patterns for authentication

---
+++ awards/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    # url(r'^new/project$',views.new_project,name='new-project'),
+    # url(r'^project/review/(\d+)',views.project_review,name='project_review'),
+    # url(r'^search/',views.search_project, name='search_results'),
+    # url(r'^profile/$',views.profile,name='profile'),
+    # url(r'^new_profile/$',views.new_profile,name = 'new_profile'),
+    # url(r'^edit/profile/$',views.profile_edit,name = 'edit_profile'),
+    # url(r'^api/profile/$', views.ProfileList.as_view()),
+    # url(r'^api/projects/$', views.ProjectList.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

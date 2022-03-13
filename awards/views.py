14ae35fa6d274c12122b5750ee8c94e8b20382f8
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-13 22:58:29 +0300

    created login and register templates

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    # all_projects = Project.fetch_all_images()
+    # return render(request,"index.html",{"all_images":all_projects})
+    return render(request, 'index.html')

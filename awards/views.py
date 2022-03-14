74e99205a00d0836e3e62bb08a5815f790a62c03
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-14 06:20:57 +0300

    registered models in admin.py

--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Project,Profile
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home(request):
-    # all_projects = Project.fetch_all_images()
-    # return render(request,"index.html",{"all_images":all_projects})
-    return render(request, 'index.html')
+    all_projects = Project.fetch_all_images()
+    return render(request,"index.html",{"all_images":all_projects})
+   

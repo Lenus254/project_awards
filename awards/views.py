e3ddbc94bc8c4bedbfc5e2b0fe221b107c0518c1
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-16 18:12:42 +0300

    styled home page

--- awards/views.py
+++ awards/views.py
@@ -140,6 +140,6 @@ class ProfileList(APIView):
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.rrors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
